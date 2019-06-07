a57e0adf0591d738525f98ab977a854f05a5bffd
JonKing93 <jonking93@gmail.com>
2019-06-07 10:33:56 -0700

    Updated the support function.

--- scaleColorMap.m
+++ scaleColorMap.m
@@ -48,13 +48,22 @@ function[varargout] = scaleColorMap( cmap, x0, ax, clim, setVals )
 %       0 and 1. (nColors x 3)
 
 % ----- Written By -----
-% Jonathan King, University of Arizona, 2019
-%
-% ----- License -----
-%
+% Jonathan King, University of Arizona, 2018
 
+% Initialize unset input arguments
+if ~exist('ax','var')
+    ax = gca;
+end
+if ~exist('clim', 'var')
+    clim = [];
+end
+if ~exist('setVals','var')
+    setVals = true;
+end
 
-% Error checking and setup
+% Do some error checking and get the color limits
+[clim] = setup( cmap, x0, ax, clim, setVals, nargout );
+
 
 
 % Get the deviation of each color limit from the centering value
@@ -77,7 +86,7 @@ end
 maxDev = max( abs( dev ) );
 
 % Get the percent of the maximum deviation associated with each limit
-percDev = dev ./ max(dev);
+percDev = dev ./ maxDev;
 
 % Get the number of color points on each half of the colormap
 halfStep = floor( size(cmap,1) / 2 );
@@ -89,6 +98,7 @@ cmap( end-nTrim+1:end, : ) = [];
 % Trim the top (minimum values) of the colormap
 nTrim = halfStep - round( percDev(1)*halfStep );
 cmap( 1:nTrim, : ) = [];
+
 
 
 % Set the values on the axes if desired
@@ -103,6 +113,84 @@ end
 varargout = {};
 if nargout == 1
     varargout = {cmap};
+end
+
+end
+
+%% Helper function to do error checking and get the color limits
+function[clim] = setup( cmap, x0, ax, clim, setVals, nOut )
+
+% Check that the colormap is allowed
+if ~ismatrix(cmap) || size(cmap,2)~=3 || ~isnumeric(cmap) || ~isreal(cmap)
+    error('cmap must be a three-column matrix of real, numeric values.');
+elseif any( isnan(cmap(:)) )
+    error('cmap may not contain NaN.');
+elseif any( cmap(:)<0 | cmap(:)>1 )
+    error('The values in cmap must be between 0 and 1.');
+end
+
+% Check that the centering value is allowed
+if ~isscalar(x0) || ~isnumeric(x0) || ~isreal(x0)
+    error('x0 must be a real, numeric, scalar value.');
+end
+
+% Check that ax is a set of axes
+if ~isa(ax, 'matlab.graphics.axis.Axes')
+    error('ax must be a set of axes handles.');
+end
+
+% If the color limits are not specified
+if isempty(clim)
+    
+    % Initialize the color limits
+    clim = [Inf, -Inf];
+    
+    % For each axes handle
+    for a = 1:numel(ax)
+        
+        % Get the color limit on this axis
+        axclim = get(ax(a), 'clim');
+        
+        % Ensure that the color limits are finite
+        if any(isinf(axclim))
+            axStr = sprintf('Axis %.f', a);
+            if isequal( ax(a), gca )
+                axStr = 'The current axis';
+            end
+            error('%s has infinite color limits. Please set them to finite values.', axStr);
+        end
+        
+        % If the axes limits are greater / smaller than the current color
+        % limits, update the color limits
+        if axclim(1) < clim
+            clim(1) = axclim(1);
+        end
+        if axclim(2) > clim
+            clim(2) = axclim(2);
+        end
+    end
+    
+% Otherwise, error check user-specified color limits
+else
+    if ~isvector(clim) || numel(clim)~=2 || ~isnumeric(clim) || ~isreal(clim)
+        error('clim must be a 2 element vector of real, numeric values.');
+    elseif any(isnan(clim))
+        error('clim cannot contain NaN.');
+    elseif any( isinf(clim) )
+        error('clim cannot be infinite.');
+    elseif clim(1)>=clim(2)
+        error('The first element in clim must be smaller than the second element of clim.');
+    end
+end
+
+% Ensure that setVals is a scalar logical
+if ~isscalar(setVals) || ~islogical(setVals)
+    error('setVals must be a scalar logical.');
+end
+
+% Check that the user did not request too many outputs
+if nOut > 1
+    error('Too many outputs.');
 end
 
 end
